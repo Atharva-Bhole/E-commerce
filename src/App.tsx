db267f36180b9f1dea6223ce57f107503e2a01e2
Atharva Bhole <atharvabhole239@gmail.com>
2024-07-11 04:55:58 +0000

    Create Template of Admin Panel with Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Suspense, lazy } from "react"
+import Loader from "./components/Loader"
+
+const Dashboard = lazy(()=> import("./pages/Dashboard"))
+const Transaction = lazy(() => import("./pages/Transaction"))
+const Customers = lazy(()=>import("./pages/Customers"))
+const Products = lazy(()=>import("./pages/Products"))
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Suspense fallback={<Loader/>}>
+        <Routes>
+          <Route path="/admin/dashboard" element={ <Dashboard/> } />
+          <Route path="/admin/products" element={ <Products/> } />
+          <Route path="/admin/transaction" element={ <Transaction/> } />
+          <Route path="/admin/customers" element={ <Customers/> } />
+
+          {/* Charts */}
+
+          
+
+          {/* Apps */}
+
+        </Routes>
+        </Suspense>
+      </Router>
+
+    </div>
   )
 }
 
